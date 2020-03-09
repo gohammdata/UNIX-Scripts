7cade6d0fc6ca355abcdb4f4660132f72122e3a9
hammdata <gohammdata@gmail.com>
2020-03-09 19:42:46 -0400

    Comments added to Connected User Script

--- basic_UNIX_script.sh
+++ basic_UNIX_script.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
+# This script clears the terminal, displays a greeting and gives information
+# about currently connected users. The two example variables are set and displayed.
 
-clear
+clear #clear terminal window
 
 echo "The script starts now."
 
-echo "Hi, $USER!"
+echo "Hi, $USER!" #Content of USER variable grabbed with the dollar sign
 echo
 
 echo "I will now fetch you a list of connected users: "
 echo
-w
-echo
+w       #show who is logged on and
+echo    #what they are doing
 
 echo "I'm setting two variables now."
-COLOR="blue"
-VALUE="9"
-echo "This is a string: $COLOR"
-echo "And this is a number: $VALUE"
+COLOR="blue"      #local shell variable string
+VALUE="9"         #local shell variable int
+echo "This is a string: $COLOR"   #display string content
+echo "And this is a number: $VALUE" #display int content
 echo
 
 echo "I'm giving you back your prompt now."
