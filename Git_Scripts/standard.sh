817f8e5a63fee13ce94da93ae42287db962457a5
hammdata <gohammdata@gmail.com>
2020-03-12 20:22:06 -0400

    Committing with the UNIX Script

--- Git_Scripts/standard.sh
+++ Git_Scripts/standard.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # Work smart, not hard
-cd ..
-cd .git
+
 GIT='git'
 echo "what is the commit message?"
 read MESSAGE
-${GIT} add
+${GIT} add .
 ${GIT} commit -m "$MESSAGE"
 ${GIT} push origin master
